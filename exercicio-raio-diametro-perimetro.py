ef87037abd42147a7b751f9689a79efcbdd8d774
SamanthaCavalcanti <samanthaccavalcanti@hotmail.com>
2025-04-04 13:33:17 -0300

    Add files via upload
    Exercicio Renan- feito em sala

---
+++ exercicio-raio-diametro-perimetro.py
@@ -0,0 +1,11 @@
+#Calculo da esfera
+r = float(input('Digite seu Raio:'))
+pi = 3.14
+
+perimetro = 2*pi*r
+area = pi* (r**2)
+diametro = 2*r
+
+print ('Seu perimetro é :',perimetro)
+print('Sua área é :' , area)
+print ('Seu diametro é: ',diametro)
